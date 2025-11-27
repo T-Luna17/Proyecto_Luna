91eb9851274b810128a3807836bbd790b8876d05
T-Luna17 <tamluna.17.fwd@gmail.com>
2025-11-27 01:36:03 -0600

    commit

--- BE/voluntariados/urls.py
+++ BE/voluntariados/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import VoluntariadoListCreateView
+from .views import VoluntariadoListCreateView, VerVoluntariosDeVoluntariados
 
 
 urlpatterns = [
     path("crear-voluntariado/", VoluntariadoListCreateView.as_view(), name="voluntariado-list-create"),
+    path("ver-voluntarios/", VerVoluntariosDeVoluntariados.as_view(), name="ver-voluntarios"),
 ]
